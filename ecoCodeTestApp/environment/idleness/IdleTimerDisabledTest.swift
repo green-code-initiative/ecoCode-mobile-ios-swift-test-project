ce1934243306340076895dcd46a24066cfa99760
Gilles Grousset <gilles.grousset@insideapp.fr>
2023-04-05 17:22:59 +0200

    separated source files for each rule check

--- ecoCodeTestApp/environment/idleness/IdleTimerDisabledTest.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import SwiftUI
-
-final class IdleTimerDisabledTest: NSObject, UIApplicationDelegate {
-    func myFunction() {
-        UIApplication.shared.isIdleTimerDisabled = true; // Issue {Idleness: Idle Timer Disabled}
-        UIScreen.main.brightness = CGFloat(0.5); // Issue {Brightness: Brightness Override} 
-    }
-}
